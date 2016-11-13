f5934b3d09d146a848f0fa4b1d6883a30f3fa0ed
Siddhesh Mhatre <sid1793@gmail.com>
2016-11-12 19:05:08 -0500

    Add optimal solution recover method to knapsack and seq alignment

--- DP/knapsack.py
+++ DP/knapsack.py
@@ -1,7 +1,7 @@
 # Knapsack algorithm
 def knapsack(values, size, capacity):
 
-	# Create matrix of size n * capacity where n is # of items
+	# Create matrix of size (n+1) * (capacity+1) where n is # of items
 	V =[ [0] * (capacity + 1) for i in range (len(values)+1)]
 
 	for i in range(0,len(values)):
@@ -11,7 +11,20 @@ def knapsack(values, size, capacity):
 			else:
 				V[i+1][j] = V[i][j]
 
+	knapsack_items(V, values, size, len(values), capacity)
+
 	return V[len(values)][capacity]
+
+def knapsack_items(V, values, size, n, cap):
+	
+	# Recursively obtain knapsack items
+	if n == 0 or cap == 0:
+		return 
+	if V[n][cap] == V[n - 1][cap - size[n-1]] + values[n-1]:
+		print 'Item:{}'.format(n)
+		knapsack_items(V, values, size, n-1, cap - size[n-1])
+	else:
+		knapsack_items(V, values, size, n-1, cap)
 
 def main():
 	values, size, capacity = [3,2,4,4], [4,3,2,3], 6
--- DP/sequence_alignment.py
+++ DP/sequence_alignment.py
@@ -15,10 +15,59 @@ def seq_align(X,Y):
 		for j in range(len(Y)):
 			A[i+1][j+1] = min((p_mismatch if X[i]!=Y[j] else 0) + A[i][j], p_gap + A[i][j+1], p_gap + A[i+1][j])
 
+	list_x = []
+	list_y = []
+	obtain_alignment(A, X, Y, len(X), len(Y), list_x, list_y)
+
+	list_x.reverse()
+	list_y.reverse()
+
+	print ''.join(list_x) + '\n' + ''.join(list_y)
+
 	return A[len(X)][len(Y)]
 
+def obtain_alignment(A, X, Y, x_idx, y_idx, list_x, list_y):
+
+	# Handle base cases
+	if x_idx == 0 and y_idx == 0:
+		return 
+
+	if x_idx == 0:
+		y_idx -= 1
+		while y_idx >= 0:
+			list_x.append('-')
+			list_y.append(Y[y_idx])
+			y_idx -= 1
+		return 
+
+	if y_idx == 0:
+		x_idx -= 1
+		while x_idx >= 0:
+			list_y.append('-')
+			list_x.append(X[x_idx])
+			x_idx -= 1
+		return
+
+	# Recursively compute optimal sequence alignment
+	p_gap = 6
+	p_mismatch = 5 if X[x_idx-1] != Y[y_idx-1] else 0
+	if A[x_idx][y_idx] == p_mismatch + A[x_idx-1][y_idx-1]:
+		list_x.append(X[x_idx-1])
+		list_y.append(Y[y_idx-1])
+		obtain_alignment(A, X, Y, x_idx-1, y_idx-1, list_x, list_y)
+	elif A[x_idx][y_idx] == p_gap + A[x_idx - 1][y_idx]:
+		list_x.append(X[x_idx - 1])
+		list_y.append('-')
+		obtain_alignment(A, X, Y, x_idx-1, y_idx, list_x, list_y)
+	else:
+		list_x.append('-')
+		list_y.append(Y[y_idx - 1])
+		obtain_alignment(A, X, Y, x_idx, y_idx-1, list_x, list_y)
+
 def main():
-	print seq_align('AGGGCT','AGGCA')
+	print 'Test case 1 -  {}'.format(seq_align('AGGGCT','AGGCA'))
+	print 'Test case 2 -  {}'.format(seq_align('','AGGCA'))
+	print 'Test case 3 -  {}'.format(seq_align('AGGGCT',''))
 
 if __name__ == "__main__":
 	main()
